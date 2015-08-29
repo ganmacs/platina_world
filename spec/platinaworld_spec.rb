d2df9b98143fd20a4ee06bd4a34832940e536b20
ganmacs <ganmacs@gmail.com>
2015-08-29 15:32:19 +0900

    setup

--- spec/platinaworld_spec.rb
+++ spec/platinaworld_spec.rb
@@ -1,11 +1,11 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Platinaworld do
-  it 'has a version number' do
+  it "has a version number" do
     expect(Platinaworld::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
+  it "does something useful" do
     expect(false).to eq(true)
   end
 end
