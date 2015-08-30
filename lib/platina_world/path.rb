4b6798e6cc11934e50263ea20b1fd09d14340e57
ganmacs <ganmacs@gmail.com>
2015-08-31 00:12:21 +0900

    Add Document

--- lib/platina_world/path.rb
+++ lib/platina_world/path.rb
@@ -6,42 +6,47 @@ module PlatinaWorld
       @file_path = file_path
     end
 
+    # @return [String] file name
     def file_name
       path[:file]
     end
 
+    # @return [String] directory path
     def directory_name
       path[:directory]
     end
 
+    # @return [True, False] path is directory or not
     def directory?
       file_name == ""
     end
 
+    # @return [True, False] path has directory path or not
     def has_directory?
       directory_name != ""
     end
 
     private
 
+    def path
+      @path ||= file_and_directory_name
+    end
+
     # Return file name and dirctory name as Array
-    # @params [String] file path (e.g "a/b/c")
-    # @return [[String, String]] first is file path name and second is directory path name
+    # @return [{String => String}] file name and directory name as Hash
     #
     # Examples.
-    # file_and_dir_name("a/b/c") => ["a/b", "c"]
-    # file_and_dir_name("a/b/c/") => ["a/b/c", ""]
-    def path
-      @path ||= begin
-        path = file_path.split("/", -1)
-        file_name = path.pop
-        directory_name = path.join("/")
+    # file_path = "a/b/c"
+    # file_and_directory_name => { directory: "a/b", file: "c"}
+    #
+    # file_path = "a/b/c/"
+    # file_and_directory_name("a/b/c/") => ["a/b/c", ""]
+    def file_and_directory_name
+      path = file_path.split("/", -1)
+      file_name = path.pop
+      directory_name = path.join("/")
 
-        {
-          file: file_name,
-          directory: directory_name
-        }
-      end
+      { file: file_name, directory: directory_name }
     end
   end
 end
